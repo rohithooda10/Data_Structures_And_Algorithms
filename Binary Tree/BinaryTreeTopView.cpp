296856f2f2814b2c3c811aecb0074f7ed79d8e50
Rohit Hooda <rhooda410@gmail.com>
2022-07-21 12:35:35 +0530

    Binary tree top,bottom view

---
+++ Binary Tree/BinaryTreeTopView.cpp
@@ -0,0 +1,101 @@
+//Binary Tree Top View
+//Code: 
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<int,int> result;
+
+struct Node
+{
+    int val;
+    Node* left;
+    Node* right;
+    Node(int data)
+    {
+        val=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+struct Node* insert(struct Node* root,int data)
+{
+    if(root==NULL)
+    {
+        root = new Node(data);
+        
+    }
+    else if(root->val>data)
+    {
+        root->left = insert(root->left,data);
+    }
+    else
+    {
+        root->right = insert(root->right,data);
+    }
+    return root;
+}
+
+void topView(struct Node* root,int horizontal)
+{
+    vector<pair<struct Node*,int>> q;
+    q.push_back(make_pair(root,horizontal));
+    result.insert({horizontal,root->val});
+    while(q.size()>0)
+    {
+        struct Node* temp = q.front().first;
+        int temp_horizontal = q.front().second;
+        q.erase(q.begin());
+        
+        if(temp->left!=NULL)
+        {
+            q.push_back(make_pair(temp->left,temp_horizontal-1));
+            if(result.find(temp_horizontal-1)==result.end())
+            {
+                result.insert({temp_horizontal-1,temp->left->val});
+            }
+        }
+        if(temp->right!=NULL)
+        {
+            q.push_back(make_pair(temp->right,temp_horizontal+1));
+            if(result.find(temp_horizontal+1)==result.end())
+            {
+                result.insert({temp_horizontal+1,temp->right->val});
+            }
+        }
+        
+        
+    }
+    
+}
+
+int main()
+{
+    int level=0;
+    struct Node* root = new Node(25);
+    root = insert(root,22);
+    root = insert(root,29);
+    root = insert(root,20);
+    root = insert(root,24);
+    root = insert(root,27);
+    root = insert(root,32);
+    
+    topView(root,0);
+    vector<int> keyList;
+    for(auto i:result)
+    {
+        keyList.push_back(i.first);
+    }
+    sort(keyList.begin(),keyList.end());
+    
+    for(int i=0;i<keyList.size();i++)
+    {
+        cout<<result[keyList[i]]<<" ";
+    }
+    
+    
+    
+    return 0;
+}
