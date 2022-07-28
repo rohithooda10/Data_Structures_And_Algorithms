1b0b23611af5bba2721482f81e9b261b090d0fd9
Rohit Hooda <rhooda410@gmail.com>
2022-07-28 11:46:23 +0530

    gcd

---
+++ Recursion/gcd.cpp
@@ -0,0 +1,24 @@
+//GCD
+//Complexity - Time = O(log(min(a,b)))
+//Code:
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a,int b)
+{
+    if(a%b==0)
+        return b;
+    else
+        return gcd(b,a%b);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<"GCD:"<<gcd(a,b);
+
+    return 0;
+}
